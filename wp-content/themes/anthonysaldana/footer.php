1798ce43243ec7a7de6f495ff4b215f299c6aaf4
vagrant <vagrant@homestead>
2015-08-17 10:07:24 +0000

    Footer Fixes and theme information  updates on style.css

--- wp-content/themes/anthonysaldana/footer.php
+++ wp-content/themes/anthonysaldana/footer.php
@@ -11,19 +11,14 @@
 ?>
 
 	</div><!-- .site-content -->
-	<footer id="colophon" class="site-footer" role="contentinfo" style="background-color: black;">
+	<footer id="colophon" class="site-footer" role="contentinfo" style="background-color: black; padding:25px;">
 	<div class="container">
 	<div class="row">
 	<div class="col-sm-4"><?php get_sidebar('content');?></div>
 		<div class="col-sm-4"><header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<?php
-						if ( is_front_page() && is_home() ) : ?>
-							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php else : ?>
-							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php endif;
-	
+							<p class="site-title "><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
 						$description = get_bloginfo( 'description', 'display' );
 						if ( $description || is_customize_preview() ) : ?>
 							<p class="site-description"><?php echo $description; ?></p>
@@ -47,7 +42,7 @@
 					 */
 					do_action( 'twentyfifteen_credits' );
 				?>
-				<p style="text-align:center;">Anthony Saldana | Web Developer</p>
+				<p style="text-align:center;"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			</div><!-- .site-info -->
 	</div>
 	
